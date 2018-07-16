ea76c3423609ce99d8953f831180fb5d996fbe4c
aakashkulkarni <kashakoolsmart@gmail.com>
2018-07-16 08:45:22 -0600

    finished clear form functionality

--- src/components/card.js
+++ src/components/card.js
@@ -3,31 +3,32 @@ import React, { Component } from 'react';
 import Input from './input';
 import Content from './content';
 
+const INITIAL_STATE = {
+        color: '',
+        pluralNoun: '',
+        adjectiveOne: '',
+        celebOne: '',
+        adjectiveTwo: '',
+        nounOne: '',
+        numberOne: '',
+        numberTwo: '',
+        nounTwo: '',
+        adjectiveThree: '',
+        celebTwo: '',
+        celebThree: '',
+        adjectiveFour: '',
+        nounThree: '',
+        celebFour: '',
+        adjectiveFive: '',
+        contentVisible: false
+    }
+
 class Card extends Component {
 
     constructor() {
         super()
 
-        this.state = {
-            color: '',
-            pluralNoun: '',
-            adjectiveOne: '',
-            celebOne: '',
-            adjectiveTwo: '',
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',
-            nounTwo: '',
-            adjectiveThree: '',
-            celebTwo: '',
-            celebThree: '',
-            adjectiveFour: '',
-            nounThree: '',
-            celebFour: '',
-            adjectiveFive: '',
-            contentVisible: false
-
-        }
+        this.state = INITIAL_STATE;
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -39,7 +40,12 @@ class Card extends Component {
 
     handleFormSubmit(event) {
         event.preventDefault()
-        this.setState({contentVisible: !this.state.contentVisible});
+
+        if (this.state.contentVisible) {
+            this.setState(INITIAL_STATE)
+        } else {
+            this.setState({contentVisible: true});
+        } 
     }
 
     render() {
@@ -68,7 +74,7 @@ class Card extends Component {
 
         return (
             <form onSubmit={this.handleFormSubmit} className="card">
-                <div classname="card__inputs">
+                <div className="card__inputs">
                 {
                     inputData.map(data => Input( (data), this.handleInputChange ))
                 }
